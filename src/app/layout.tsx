aeed823be91ceb250859995145f0be8f5a27e79d
Gia Miminoshvili <130286737+giamimino@users.noreply.github.com>
2026-06-14 21:17:57 +0000

    feat: shadcn was intalled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Nunito_Sans, Outfit } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const outfitHeading = Outfit({subsets:['latin'],variable:'--font-heading'});
+
+const nunitoSans = Nunito_Sans({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "QuickIssue",
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", nunitoSans.variable, outfitHeading.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
