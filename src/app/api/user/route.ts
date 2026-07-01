2ca68cb03faad4bfc1c89658f840b0b9255da749
giamimino <miminoshvili2102@gmail.com>
2026-07-01 16:47:59 +0400

    feat(user): add error handling to user GET endpoint and extand endpoint with getting user

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,23 +1,29 @@
+import GENERIC_ERRORS from "@/constants/errors/generic.errors";
+import { sql } from "@/lib/db";
 import { ERROR_TYPE } from "@/types/generic";
 import { NextResponse } from "next/server";
 
 export async function GET(
   req: Request,
-): Promise<NextResponse<{ ok: boolean; error?: ERROR_TYPE }>> {
+): Promise<NextResponse<{ ok: boolean; error?: ERROR_TYPE; user?: unknown }>> {
   try {
     const { searchParams } = new URL(req.url);
     const { id } = Object.fromEntries(searchParams.entries());
 
     if (!id || typeof id !== "string")
       return NextResponse.json(
-        { ok: false, error: { title: "", description: "" } },
+        { ok: false, error: GENERIC_ERRORS.GENERIC_ERROR },
         { status: 400 },
       );
-    return NextResponse.json({ ok: true }, { status: 200 });
+
+    const users = await sql.query(`SELECT * FROM users WHERE id = $1`, [id]);
+    const user = users[0];
+
+    return NextResponse.json({ ok: true, user }, { status: 200 });
   } catch (err) {
     console.error(err);
     return NextResponse.json(
-      { ok: false, error: { title: "", description: "" } },
+      { ok: false, error: GENERIC_ERRORS.SERVER_ERROR },
       { status: 500 },
     );
   }
