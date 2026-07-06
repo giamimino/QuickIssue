6f861402b76d486092057cd21b91ada253a16d4a
giamimino <miminoshvili2102@gmail.com>
2026-07-06 17:56:40 +0400

    feat(help): help category cache moved from memory to session storage, added hook for session storage and removed memory cache

---
+++ src/hooks/useHelpCache.ts
@@ -0,0 +1,34 @@
+import { HelpCenterCategoryType } from "@/types/help-center";
+import safeParse from "@/utils/safeParse";
+import { useEffect, useState } from "react";
+
+const STORAGE_KEY = "help-center/categories";
+
+export default function useHelpCache() {
+  const [categories, setCategories] = useState<HelpCenterCategoryType[]>([]);
+
+  useEffect(() => {
+    const getData = () => {
+      const data = sessionStorage.getItem(STORAGE_KEY);
+
+      if (!data) return;
+
+      const parsed = safeParse<HelpCenterCategoryType[], null>(data, null);
+
+      if (parsed && parsed.length !== 0) {
+        setCategories(parsed);
+      }
+    };
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(categories));
+  }, [categories]);
+
+  return {
+    categories,
+    setCategories,
+  };
+}
--- src/lib/cache/help/help-category.cache.ts
+++
@@ -1,7 +0,0 @@
-import { HelpCenterCategoryType } from "@/types/help-center";
-
-export const HelpCategoryCache = new Map<string, HelpCenterCategoryType>();
-
-export function GetCategoryFromHelpCache(slug: string) {
-  return HelpCategoryCache.get(slug);
-}
