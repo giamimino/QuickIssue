7a908a0c6d2dd416b18aa3feed08840d0d1d6f55
giamimino <miminoshvili2102@gmail.com>
2026-07-07 23:56:30 +0400

    feat(help/layout): add sidebar importing

--- src/app/help/layout.tsx
+++ src/app/help/layout.tsx
@@ -1,7 +1,15 @@
+import HelpSidebar from "@/components/layouts/help-sidear";
 import React from "react";
 
 const HelpLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div>
+        <HelpSidebar />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default HelpLayout;
