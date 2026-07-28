6625b88ac65d33c5d18e7abad54b2931697573d8
giamimino <miminoshvili2102@gmail.com>
2026-07-28 14:53:57 +0400

    feat(404): add footer to 404 error page and add mobile/tablet responsive

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,20 +1,23 @@
 import { Header } from "@/components/layouts/header";
+import { Footer } from "@/components/sections/landing/footer";
 import { CardDescription, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 import React from "react";
 
 export default function NotFound() {
   return (
-    <div className="bg-background w-full h-screen p-12 flex flex-col gap-2.5">
+    <div className="bg-background w-full pt-24 sm:p-12 p-6 flex flex-col gap-2.5">
       <Header />
-      <div className="h-screen flex justify-center flex-col gap-2.5">
+      <div className="flex sm:h-screen h-fit sm:justify-center flex-col gap-2.5">
         <CardDescription className="text-xs">ERROR CODE 404</CardDescription>
-        <CardTitle className="text-6xl font-bold">OOOPS!!</CardTitle>
-        <CardTitle className="text-3xl font-normal max-w-90">
+        <CardTitle className="text-9xl font-bold max-md:text-8xl max-sm:text-6xl">
+          OOOPS!!
+        </CardTitle>
+        <CardTitle className="text-3xl font-normal max-w-90 max-sm:text-lg">
           This is not the page you are looking for
         </CardTitle>
 
-        <div className="mt-10 flex flex-col gap-2.5">
+        <div className="md:mt-10 mt-5 flex flex-col gap-2.5">
           <CardDescription>
             Here are some helpful links instead:
           </CardDescription>
@@ -33,6 +36,9 @@ export default function NotFound() {
             </Link>
           </div>
         </div>
+      </div>
+      <div className="sm:mt-0 mt-20">
+        <Footer />
       </div>
     </div>
   );
